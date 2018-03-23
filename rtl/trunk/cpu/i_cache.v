d63ad2353a50b39534c26490c89c54ec667e42f3
Madhuri Gontala <mads3009@gmail.com>
2018-03-23 01:20:03 -0500

    made changes to icache

--- rtl/trunk/cpu/i_cache.v
+++ rtl/trunk/cpu/i_cache.v
@@ -9,8 +9,9 @@
 /* Author: Madhuri Gontala                              */
 /********************************************************/
 
-module i_cache(oe, index, tag_14_12, tag_11_9, ic_fill_data, ic_miss_ack, v_init, r_data, r_ready, ic_miss, ic_addr);
-    input         oe;
+module i_cache(clk, ren, index, tag_14_12, tag_11_9, ic_fill_data, ic_miss_ack, v_init, r_data, r_ready, ic_miss, ic_addr);
+    input         clk;
+    input         ren;
     input  [3:0]  index;
     input  [2:0]  tag_14_12; 
     input  [2:0]  tag_11_9; 
@@ -28,15 +29,20 @@ module i_cache(oe, index, tag_14_12, tag_11_9, ic_fill_data, ic_miss_ack, v_init
     wire [5:0] phy_tag;
     
     assign phy_tag = {tag_14_12,tag_11_9};
-    
-    assign ic_miss = ~hit && ~oe;
+   
+    wire nothit;
+    NOT u_not_hit (hit, not_hit); 
+    //assign ic_miss = ~hit && ren;
+    and2$ u_ic_miss(ic_miss, not_hit, ren);
+
     assign ic_addr = {phy_tag,index,5'b0};
     
-    assign r_ready = hit && ~oe;
+    and2$ u_r_ready (r_ready, hit, ren);
     
     data_store ds(
+        .clk    (clk),
         .index  (index),
-        .oe     (oe),
+        .oe     (1'b1),
         .fill_data  (ic_fill_data),
         .wr   (ic_miss_ack),
         .r_data     (r_data)
@@ -46,23 +52,24 @@ module i_cache(oe, index, tag_14_12, tag_11_9, ic_fill_data, ic_miss_ack, v_init
     wire valid;
     
     tag_store ts(
+        .clk    (clk),
         .index (index),
-        .oe(oe),
+        .oe(1'b1),
         .tag (tag_in_cache),
-        .valid (valid),
+        .tag_valid (tag_valid),
         .wr (ic_miss_ack),
         .wr_tag(phy_tag),
         .v_init(v_init)
     );
     
-    wire tag_match;
-    assign tag_match = (tag_in_cache == phy_tag);
+    wire tag_match, tag_valid;
+    eq_checker #6 (tag_in_cache, phy_tag, tag_match);
     
-    assign r_ready = tag_match && hit && ~oe;
+    and2$ u_hit (hit, tag_match, tag_valid);
 
 endmodule
  
-module ramnB8w(A, DIN, WR, OE, DOUT);
+module ram_nB_8w(A, DIN, WR, OE, DOUT);
     parameter N = 32;
     input [2:0] A;
     input [8*N-1:0] DIN ;
@@ -79,14 +86,18 @@ endmodule
 
 
 module tag_store(
+    input clk,
     input [3:0] index,
     input       oe,
     input [5:0] wr_tag,
     input       wr,
     input       v_init,
     output [5:0] tag,
-    output       valid
+    output       tag_valid
     );
+
+    wire w_wr;
+    and2$ (w_wr,clk,wr);
 
     wire wr_upper, wr_lower;
     wire   [5:0] tag_upper;
@@ -94,44 +105,42 @@ module tag_store(
     
     wire   [3:0] blah;
     
-    assign wr_upper = wr && index[3];
-    assign wr_lower = wr && ~index[3];
-    
+    wire index_lower;
+    NOT u_indexlower (index[3], index_lower);
+    and2$ u_wr_upper (w_wr, index[3], wr_upper);
+    and2$ u_wr_lower (w_wr, index_lower, wr_lower);
+
     ram8b8w$ ts_lower (.A(index[2:0]), .DIN({2'b0,wr_tag}), .OE(oe), .WR(wr_lower), .DOUT({blah[1:0],tag_lower}));
     ram8b8w$ ts_upper (.A(index[2:0]), .DIN({2'b0,wr_tag}), .OE(oe), .WR(wr_upper), .DOUT({blah[3:2],tag_upper}));
     
-    assign tag = index[3] ? tag_upper : tag_lower;
+    //FIXME : fanout 6;
+    mux_nbit_2x1 #6 u_tag (tag_lower, tag_upper, index[3], tag);
 
-    //latches for valid;
+    //dffs for valid;
     wire [15:0] enables;
-    wire tag_v_in;
+    wire [15:0] tag_v_in;
     wire [15:0] tag_vs;
     wire [15:0] one_hot_wr;
 
     assign enable = v_init | wr;
     demux_1x16 demux (1'b1, index ,one_hot_wr);    
-    assign tag_v_in = (v_init) ? 1'b0 : 1'b1;
+    assign tag_v_in = (v_init) ? 16'b0 : 16'hffff;
     assign enables = (v_init) ? 16'hffff : one_hot_wr;
 
-    genvar i;
-    generate begin : loop
-    for (i=0; i<16; i=i+1) begin
-    latch$ valids(
-        .en(enables[i]),
-        .d(tag_v_in),
-        .q(tag_vs[i]),
-        .r(1'b1),
-        .s(1'b1),
-        .qbar ()
+    dff16$ u_valids(
+        .CLK(clk),
+        .D(tag_v_in),
+        .Q(tag_vs),
+        .CLR(1'b1),
+        .PRE(1'b1),
+        .QBAR ()
     );
-    end
-    end
-    endgenerate
+    mux_16x1 u_valid (tag_vs, index, tag_valid);
 
-    mux_16x1 u_valid (tag_vs, index, valid);
 endmodule
 
 module data_store(
+    input           clk,
     input   [3:0]   index,
     input           oe,
     input   [255:0] fill_data,
@@ -139,17 +148,23 @@ module data_store(
     output  [255:0] r_data
     );
     
+    wire w_wr;
+    and2$ (w_wr,clk,wr);
+
     wire wr_upper, wr_lower;
     wire   [255:0] dout_upper;
     wire   [255:0] dout_lower;
     
-    assign wr_upper = wr && index[3];
-    assign wr_lower = wr && ~index[3];
+    wire index_lower;
+    NOT u_indexlower (index[3], index_lower);
+    and2$ u_wr_upper (w_wr, index[3], wr_upper);
+    and2$ u_wr_lower (w_wr, index_lower, wr_lower);
     
-    ramnB8w upper_ram (.A(index[2:0]), .DIN(fill_data), .OE(oe), .WR(wr_upper), .DOUT(dout_upper));
-    ramnB8w lower_ram (.A(index[2:0]), .DIN(fill_data), .OE(oe), .WR(wr_lower), .DOUT(dout_lower));
+    ram_nB_8w upper_ram (.A(index[2:0]), .DIN(fill_data), .OE(oe), .WR(wr_upper), .DOUT(dout_upper));
+    ram_nB_8w lower_ram (.A(index[2:0]), .DIN(fill_data), .OE(oe), .WR(wr_lower), .DOUT(dout_lower));
     
-    assign r_data = index[3] ? dout_upper : dout_lower;
+    //FIXME : Huge fanout for sel;
+    mux_nbit_2x1 #256 (dout_lower, dout_upper, index[3], r_data);
 
 endmodule
 
