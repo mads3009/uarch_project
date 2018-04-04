df2a5c6a030b72268516633168c602fd5e81242c
Madhuri Gontala <mads3009@gmail.com>
2018-04-04 01:19:46 -0500

    reuse

---
+++ rtl/reuse/sub_designs/synth/pencoder_3_2.tcl
@@ -0,0 +1,11 @@
+set LIB_PATH ../../../../synth/lib/
+set SCRIPTS_PATH ../../../../synth/scripts/
+set HDL_PATH ./
+
+set current_design pencoder_3_2
+set myFiles [list pencoder_3_2.v]
+
+set SDC_FILE ./constraints_${current_design}.sdc
+
+source ${SCRIPTS_PATH}/rc.tcl
+
