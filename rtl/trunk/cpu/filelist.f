e17774dea61db4299b36d8a1bd359db45850a750
Madhuri Gontala <mads3009@gmail.com>
2018-04-05 00:50:22 -0500

    filelist for cpu compile

---
+++ rtl/trunk/cpu/filelist.f
@@ -0,0 +1,26 @@
+/home/projects/courses/spring_18/ee382n-16120/lib/time -v2005 -sv 
+/home/projects/courses/spring_18/ee382n-16120/lib/lib3
+/home/projects/courses/spring_18/ee382n-16120/lib/lib1
+/home/projects/courses/spring_18/ee382n-16120/lib/lib2
+/home/projects/courses/spring_18/ee382n-16120/lib/lib4
+../../reuse/shifters/mux_1bit.v
+../../reuse/sub_designs/register.v
+../../reuse/sub_designs/register_ld2bit.v
+../../reuse/adders/kogge_stone/FA.v
+../../reuse/adders/kogge_stone/kogge_stone.v
+../../reuse/sub_designs/eq_checker.v
+../../reuse/sub_designs/mux_4x1.v
+../../reuse/sub_designs/mux_8x1.v
+../../reuse/sub_designs/mux_16x1.v
+../../reuse/sub_designs/demux_1x16.v
+../../reuse/sub_designs/mux_nbit_2x1.v
+../../reuse/sub_designs/mux_nbit_4x1.v
+../../reuse/sub_designs/mux_nbit_8x1.v
+../../reuse/sub_designs/mux_nbit_8x1_mulsel.v
+../../reuse/sub_designs/ram_nB_8w.v
+../../reuse/shifters/shift_right_rotate.v
+fetch/i_cache.v
+fetch/fetch_TLB_lookup.v
+fetch/fetch_fsm.v
+EIP_reg.v
+cpu.v
