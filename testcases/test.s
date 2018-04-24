9f47d8ba5f679582e4b7f0154584997a3235ea74
Madhuri Gontala <mads3009@gmail.com>
2018-04-24 16:59:34 -0500

    moving .o,.s

--- testcases/test.s
+++
@@ -1,8 +0,0 @@
-.ORG 0X0000
-
-mov $-2, %EAX
-L1: inc %EBX
-inc %EAX
-jne L1
-inc %ECX
-hlt
--- testcases/test2.s
+++
@@ -1,8 +0,0 @@
-.ORG   0x00000000
-movl    $0xa00, %edx
-movw    %dx, %ds
-hlt
-movl    $0x0, %ebx
-addl   (%ebx), %eax 
-hlt
-
