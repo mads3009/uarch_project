af162594804d452992041459261bd85b1a5e6e07
Gayatri Sivaraman <gsivaraman@luigi.(none)>
2018-04-27 15:16:34 -0500

    Decode disp bug fixed

--- testcases/test2.s
+++ testcases/test2.s
@@ -1,15 +1,13 @@
 .ORG   0x00000000
 movl    $0xa00, %edx
 movw    %dx, %ds
-movl    $0xb00, %ecx
-movw    %cx, %gs
 movl    $0x02      , %eax
 movl    $0x01      , %ebx
 movl    $0x06      , %ebp
-movl    $0x12      , %gs:(%eax)
-movl    $0x13      , %gs:(%ebp)
+movl    $0x12      , (%eax)
+movl    $0x13      , (%ebp)
 
-addw    %dx       ,%gs:0x3(%eax,%ebx,0x2) 
+addw    %dx       ,0x3(%eax,%ebx,0x2) 
 
 hlt
 
