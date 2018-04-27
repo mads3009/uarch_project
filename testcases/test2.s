ad2b6f38c22f46f406e90d88342e7d4ef3e3480e
Gayatri Sivaraman <gsivaraman@luigi.(none)>
2018-04-27 14:25:37 -0500

    test2

--- testcases/test2.s
+++ testcases/test2.s
@@ -1,10 +1,15 @@
 .ORG   0x00000000
 movl    $0xa00, %edx
 movw    %dx, %ds
-movl    $0xAB      , %eax
-movl    $0xCD      , %ecx
+movl    $0xb00, %ecx
+movw    %cx, %gs
+movl    $0x02      , %eax
+movl    $0x01      , %ebx
+movl    $0x06      , %ebp
+movl    $0x12      , %gs:(%eax)
+movl    $0x13      , %gs:(%ebp)
 
-addb    %cl        ,(%eax) 
+addw    %dx       ,%gs:0x3(%eax,%ebx,0x2) 
 
 hlt
 
