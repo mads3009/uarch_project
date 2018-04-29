c635852effc05fbd2eb814e4abcf6565d453998f
Madhuri Gontala <mads3009@gmail.com>
2018-04-28 20:37:23 -0500

    call changes

--- testcases/test.s
+++ testcases/test.s
@@ -2,15 +2,21 @@
 movl    $0xa00, %edx
 movw    %dx, %ds
 movl    $0xb00, %ecx
-movw    %cx, %gs
+movw    %cx, %ss
 inc %ax
 mov $0x5566, %bx
 jmp L1
 movw $1234, %dx
 L1 : movw $200, %dx
-jmp $0x0200,$00000000
+
+movl    $0xAB      , %eax
+movw    $0x400      , (%eax)
+
+movl $0xff, %esp 
+call $0x0200,$00000000
 L3 : mov $5555, %edx
 
 .ORG 0x2000000
 L2: mov $3569, %esp
 hlt
+callw (%eax)
