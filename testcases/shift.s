ac512139ebf9fdadf88ffc5e9e8eae74433e13ab
Madhuri Gontala <mads3009@gmail.com>
2018-05-02 23:34:11 -0500

    testcases

--- testcases/shift.s
+++ testcases/shift.s
@@ -8,11 +8,29 @@ movw    %ax, %fs
 
 movl  $0x400, %eax
 movl  $0x01234567, (%eax)
-movl  $0x89abcdef, 0x4(%eax)
+movl  $0x89abcfef, 0x4(%eax)
 
 movl  $0x030303e3,  %ecx
 salb  $0x1, (%eax) 
 salb  %cl, 0x4(%eax) 
 salb  $0x2, 0x4(%eax)
 
+movl $0x123f5678, %ebx
+mov $0, %cl
+sarb %cl, (%eax)
+sarb $0x1, 4(%eax)
+sarb $0x2, %bh
+
+mov $0xe3, %cl
+sarw %cl, (%eax)
+sarw $0x1, (%eax)
+sarw $0x4, 0x4(%eax)
+
+salw %cl, %ebx
+salw $0x0, %ebx
+salw $0x20, %ebx
+
+mov $0xdeaddead, %eax
+
+hlt
 
